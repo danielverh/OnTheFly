3c04c08129f33ca6aa5fe403ae329c463ad4de3a
Daniel Verhoef <danielverhoef@hotmail.nl>
2019-09-29 21:12:24 +0200

    Update MathLib.cs

--- Vm/Runtime/MathLib.cs
+++ Vm/Runtime/MathLib.cs
@@ -7,6 +7,7 @@ namespace OnTheFly.Vm.Runtime
     public class MathLib : FLibrary
     {
         internal delegate float SingleFOp(float inp);
+
         public MathLib()
         {
             RegisterMethod("floor", 1, Single(MathF.Floor));
@@ -14,9 +15,14 @@ namespace OnTheFly.Vm.Runtime
             RegisterMethod("sin", 1, Single(MathF.Sin));
             RegisterMethod("cos", 1, Single(MathF.Cos));
             RegisterMethod("tan", 1, Single(MathF.Tan));
+            RegisterMethod("asin", 1, Single(MathF.Asin));
+            RegisterMethod("acos", 1, Single(MathF.Acos));
+            RegisterMethod("atan", 1, Single(MathF.Atan));
+            RegisterMethod("sqrt", 1, Single(MathF.Sqrt));
             RegisterMethod("log", 1, Single(MathF.Log));
             RegisterMethod("log10", 1, Single(MathF.Log10));
         }
+
         internal Method Single(SingleFOp op)
         {
             return x =>
@@ -24,7 +30,6 @@ namespace OnTheFly.Vm.Runtime
                 ;
                 return FObject.NewF32(op.Invoke(Is<float>(x[0])));
             };
-
         }
     }
 }
