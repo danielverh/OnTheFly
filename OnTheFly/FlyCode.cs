385814a88354b625e1058fbb5c22b302329dd5ff
danielverh <daniel@carrotdesign.nl>
2021-01-03 15:31:00 +0100

    Update FlyCode.cs

--- OnTheFly/FlyCode.cs
+++ OnTheFly/FlyCode.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,27 @@ namespace OnTheFly
             return (listener.Code.Instructions, listener.Code.Contexts);
         }
 
+        public static void WriteBytecode(Instructions instructions, Stream output)
+        {
+            using (output)
+                output.Write(instructions.ToArray());
+        }
+
+        public static Instructions ReadBytecode(Stream input)
+        {
+            Instructions instr = new Instructions();
+            using (input)
+            {
+                var buffer = new byte[1024];
+                while (input.CanRead)
+                {
+                    var read = input.Read(buffer);
+                    instr.AddRange(buffer.Take(read));
+                }
+            }
+
+            return instr;
+        }
         public static FObject RunEval(Instructions instructions, CodeContexts contexts)
         {
             VirtualMachine vm = new VirtualMachine(instructions, contexts);
